206e8d57f6a9e44011947c17354cc0e9ad3abe48
davidjones <david.jones@tnt-digital.com>
2020-04-28 15:28:33 +0200

    Remove a lot of things to make a minimal example work on server

--- app.py
+++ app.py
@@ -20,174 +20,10 @@ stacked_df_path = f'./data/result.csv'
 df = pd.read_csv(stacked_df_path, header=0)
 available_indicators = df['indicator'].unique()
 
-days = df.Days.unique()
-continents = df.Continent.unique()
-
-df_total_new_cases = df.loc[(df.indicator == 'total_cases') |
-                            (df.indicator == 'new_cases')]
-df_total_new_cases = df_total_new_cases.drop(labels=['Unnamed: 0'], axis=1)
-
-pivoted = (df_total_new_cases
-           .set_index(['Date', 'Country/Region', 'Continent', 'Days'])
-           .pivot_table(values='value',
-                        index=['Date', 'Country/Region', 'Continent', 'Days'],
-                        columns='indicator',
-                        aggfunc='mean',
-                        fill_value=0)
-           .reset_index()
-           )
-
 colors = {
     'background': '#111111',
     'text': '#7FDBFF'
 }
-
-
-def create_animation_scatter_plot() -> go.Figure:
-
-    # make figure
-    fig_dict = {
-        "data": [],
-        "layout": {},
-        "frames": []
-    }
-
-    # fill in most of layout
-    fig_dict["layout"]["xaxis"] = {"range": [0, 6.5],
-                                   "title": "Total Cases",
-                                   'type': 'log'}
-    fig_dict["layout"]["yaxis"] = {"range": [0, 5.5],
-                                   "title": "New Cases",
-                                   "type": "log"}
-    fig_dict["layout"]["hovermode"] = "closest"
-    fig_dict["layout"]["sliders"] = {
-        "args": [
-            "transition", {
-                "duration": 400,
-                "easing": "cubic-in-out"
-            }
-        ],
-        "initialValue": "0",
-        "plotlycommand": "animate",
-        "values": days,
-        "visible": True
-    }
-    fig_dict["layout"]["updatemenus"] = [
-        {
-            "buttons": [
-                {
-                    "args": [None, {"frame": {"duration": 500,
-                                              "redraw": False},
-                                    "fromcurrent": True,
-                                    "transition": {"duration": 300,
-                                                   "easing": "quadratic-in-out"
-                                                   }
-                                    }
-                             ],
-                    "label": "Play",
-                    "method": "animate"
-                },
-                {
-                    "args": [[None], {"frame": {"duration": 0,
-                                                "redraw": False
-                                                },
-                                      "mode": "immediate",
-                                      "transition": {"duration": 0}
-                                      }
-                             ],
-                    "label": "Pause",
-                    "method": "animate"
-                }
-            ],
-            "direction": "left",
-            "pad": {"r": 10, "t": 87},
-            "showactive": False,
-            "type": "buttons",
-            "x": 0.1,
-            "xanchor": "right",
-            "y": 0,
-            "yanchor": "top"
-        }
-    ]
-
-    sliders_dict = {
-        "active": 0,
-        "yanchor": "top",
-        "xanchor": "left",
-        "currentvalue": {
-            "font": {"size": 20},
-            "prefix": "Day:",
-            "visible": True,
-            "xanchor": "right"
-        },
-        "transition": {"duration": 300, "easing": "cubic-in-out"},
-        "pad": {"b": 10, "t": 50},
-        "len": 0.9,
-        "x": 0.1,
-        "y": 0,
-        "steps": []
-    }
-
-    # make data
-    day = 0
-    for Continent in continents:
-        dataset_by_day = pivoted[pivoted["Days"] == day]
-        dataset_by_year_and_cont = dataset_by_day[
-            dataset_by_day["Continent"] == Continent]
-
-        data_dict = {
-            "x": list(dataset_by_year_and_cont["total_cases"]),
-            "y": list(dataset_by_year_and_cont["new_cases"]),
-            "mode": "markers",
-            "text": list(dataset_by_year_and_cont["Country/Region"]),
-            "marker": {
-                "sizemode": "area",
-                "sizeref": 100,
-                "size": list(dataset_by_year_and_cont["total_cases"])
-            },
-            "name": Continent
-        }
-        fig_dict["data"].append(data_dict)
-
-    # make frames
-    for day in days:
-        frame = {"data": [], "name": str(day)}
-        for continent in continents:
-            dataset_by_year = pivoted[pivoted["Days"] == int(day)]
-            dataset_by_year_and_cont = dataset_by_year[
-                dataset_by_year["Continent"] == continent]
-
-            data_dict = {
-                "x": list(dataset_by_year_and_cont["total_cases"]),
-                "y": list(dataset_by_year_and_cont["new_cases"]),
-                "mode": "markers",
-                "text": list(dataset_by_year_and_cont["Country/Region"]),
-                "marker": {
-                    "sizemode": "area",
-                    "sizeref": 50,
-                    "size": list(dataset_by_year_and_cont["total_cases"])
-                },
-                "name": continent
-            }
-            frame["data"].append(data_dict)
-
-        fig_dict["frames"].append(frame)
-        slider_step = {"args": [
-            [str(day)],
-            {"frame": {"duration": 300, "redraw": False},
-             "mode": "immediate",
-             "transition": {"duration": 300}}
-        ],
-            "label": str(day),
-            "method": "animate"}
-        sliders_dict["steps"].append(slider_step)
-
-    fig_dict["layout"]["sliders"] = [sliders_dict]
-
-    return go.Figure(fig_dict)
-
-
-fig = create_animation_scatter_plot()
 
 app.layout = html.Div([
 
@@ -258,7 +94,8 @@ app.layout = html.Div([
                   'display': 'inline-block',
                   'padding': '10 10',
                   'borderRight': 'thin lightgrey solid',
-                  }
+                  },
+            id='graph'
         ),
 
         # Right-hand-side X and Y time series
@@ -286,20 +123,8 @@ app.layout = html.Div([
     ], style={
         'borderBottom': 'thin lightgrey solid',
         'backgroundColor': 'rgb(250, 250, 250)',
-        'padding': '10px 5px'
-    }
-    ),
-
-    html.Div([
-        dcc.Graph(id='total-new-cases-slider',
-                  figure=fig,
-                  style={'height': '700px'}),
-    ], style={
-        'borderBottom': 'thin lightgrey solid',
-        'backgroundColor': 'rgb(250, 250, 250)',
-        'padding': '10px 5px',
-        'vertical-align': 'middle'
-    }
+        'padding': '10px 5px'},
+        id='main-plot-div'
     ),
 
     html.Div(children=f'Copyright Dr. David I. Jones, 2020. MIT License. '
@@ -321,7 +146,7 @@ app.layout = html.Div([
      dash.dependencies.Input('crossfilter-year--slider', 'value')])
 def update_graph(xaxis_column_name, yaxis_column_name,
                  xaxis_type, yaxis_type,
-                 date_value):
+                 date_value) -> dict:
     dff = df[df['Days'] == date_value]
     return {
         'data': [dict(
@@ -350,11 +175,12 @@ def update_graph(xaxis_column_name, yaxis_column_name,
             margin={'l': 40, 'b': 30, 't': 10, 'r': 0},
             height=450,
             hovermode='closest'
-        )
+        ),
+        "id": 'blerg'
     }
 
 
-def create_time_series(dff, axis_type, title):
+def create_time_series(dff, axis_type, title) -> dict:
     return {
         'data': [dict(
             x=dff['Date'],
@@ -402,4 +228,4 @@ def update_x_timeseries(hover_data, yaxis_column_name, axis_type):
 
 
 if __name__ == '__main__':
-    app.run_server()
+    app.run_server(port=8088)
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='src',
     packages=find_packages(),
-    version='0.1.0',
+    version='0.0.1',
     description=f'A dashboard for visualising my analyses of the Johns Hopkins Univerisity\'s (JHUs)'
                 f' corona-virus dataset.',
     author='David Jones',
