2e061b3e2dd68d3a0bd5582283a43e2a46a6918c
davidjones <david.jones@tnt-digital.com>
2020-04-28 15:38:53 +0200

    Complete minimal example

--- app.py
+++ app.py
@@ -1,231 +1,29 @@
-# -*- coding: utf-8 -*-
-from datetime import datetime
-import plotly.graph_objects as go
-
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-import pandas as pd
 
-pd.set_option("display.max_columns", 500)
-pd.set_option("display.max_rows", 1000)
-pd.set_option("display.width", 1000)
+app = dash.Dash(__name__)
 
-external_stylesheets = [f'https://github.com/plotly/dash-app-stylesheets/blob/master/dash-oil-and-gas.css']
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+app.layout = html.Div(children=[
+    html.H1(children='Hello Dash'),
 
-date_today = datetime.strftime(datetime.today(), '%d-%m-%Y')
+    html.Div(children='''
+        Dash: A web application framework for Python.
+    '''),
 
-stacked_df_path = f'./data/result.csv'
-df = pd.read_csv(stacked_df_path, header=0)
-available_indicators = df['indicator'].unique()
-
-colors = {
-    'background': '#111111',
-    'text': '#7FDBFF'
-}
-
-app.layout = html.Div([
-
-    html.Title('Corona-virus Dashboard'),
-    # Dashboard heading
-    html.H1(
-        children='Corona-virus Dashboard',
-        style={
-            'textAlign': 'center',
+    dcc.Graph(
+        id='example-graph',
+        figure={
+            'data': [
+                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
+                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
+            ],
+            'layout': {
+                'title': 'Dash Data Visualization'
+            }
         }
-    ),
-
-    # Dashboard sub-heading
-    html.Div(children=f'A dashboard for visualising my analyses of the Johns Hopkins Univerisity\'s (JHUs)'
-                      f' corona-virus dataset.',
-             style={
-                 'textAlign': 'center',
-             }),
-
-    html.Div([
-        # Left-hand (X-axis) dropdown and log/linear radio buttons
-        html.Div([
-            dcc.Dropdown(
-                id='crossfilter-xaxis-column',
-                options=[{'label': i, 'value': i} for i in available_indicators],
-                value='Total Cases (cumulative cases / country)'
-            ),
-            dcc.RadioItems(
-                id='crossfilter-xaxis-type',
-                options=[{'label': i, 'value': i} for i in ['Linear', 'Log']],
-                value='Linear',
-                labelStyle={'display': 'inline-block'}
-            )
-        ], style={'width': '49%',
-                  'float': 'left',
-                  'display': 'inline-block'}
-        ),
-
-        # Right-hand (Y-axis) dropdown and log/linear radio buttons
-        html.Div([
-            dcc.Dropdown(
-                id='crossfilter-yaxis-column',
-                options=[{'label': i, 'value': i} for i in available_indicators],
-                value='New cases / day / country'
-            ),
-            dcc.RadioItems(
-                id='crossfilter-yaxis-type',
-                options=[{'label': i, 'value': i} for i in ['Linear', 'Log']],
-                value='Linear',
-                labelStyle={'display': 'inline-block'}
-            )
-        ], style={'width': '49%', 'float': 'right', 'display': 'inline-block'})
-    ], style={
-        'borderBottom': 'thin lightgrey solid',
-        'backgroundColor': 'rgb(250, 250, 250)',
-        'padding': '10px 5px'
-    }),
-
-    html.Div([
-        html.Div([
-            # Main plot
-            dcc.Graph(
-                id='crossfilter-indicator-scatter',
-                hoverData={'points': [{'customdata': 'Spain'}]},
-            )
-        ], style={'width': '49%',
-                  'float': 'left',
-                  'display': 'inline-block',
-                  'padding': '10 10',
-                  'borderRight': 'thin lightgrey solid',
-                  },
-            id='graph'
-        ),
-
-        # Right-hand-side X and Y time series
-        html.Div([
-            dcc.Graph(id='x-time-series'),
-            dcc.Graph(id='y-time-series'),
-        ], style={'display': 'inline-block',
-                  'width': '49%',
-                  'borderLeft': 'thin lightgrey solid'
-                  }
-        ),
-
-        # Slider for time movement
-        html.Div(dcc.Slider(
-            id='crossfilter-year--slider',
-            min=df['Days'].min(),
-            max=df['Days'].max(),
-            value=df['Days'].max(),
-            marks={str(year): str(year) for year in df['Days'].unique()[0::5]},
-            step=2
-        ), style={'width': '49%',
-                  'float': 'left',
-                  'padding': '0px 20px 20px 20px'}
-        ),
-    ], style={
-        'borderBottom': 'thin lightgrey solid',
-        'backgroundColor': 'rgb(250, 250, 250)',
-        'padding': '10px 5px'},
-        id='main-plot-div'
-    ),
-
-    html.Div(children=f'Copyright Dr. David I. Jones, 2020. MIT License. '
-                      f'See https://github.com/drblahdblah/covid-19-analysis for the code.',
-             style={
-                 'textAlign': 'right',
-                 'width': '100%'
-             }
-             )
+    )
 ])
 
-
-@app.callback(
-    dash.dependencies.Output('crossfilter-indicator-scatter', 'figure'),
-    [dash.dependencies.Input('crossfilter-xaxis-column', 'value'),
-     dash.dependencies.Input('crossfilter-yaxis-column', 'value'),
-     dash.dependencies.Input('crossfilter-xaxis-type', 'value'),
-     dash.dependencies.Input('crossfilter-yaxis-type', 'value'),
-     dash.dependencies.Input('crossfilter-year--slider', 'value')])
-def update_graph(xaxis_column_name, yaxis_column_name,
-                 xaxis_type, yaxis_type,
-                 date_value) -> dict:
-    dff = df[df['Days'] == date_value]
-    return {
-        'data': [dict(
-            x=dff[(dff['indicator'] == xaxis_column_name) & (dff['Continent'] == i)]['value'],
-            y=dff[(dff['Continent'] == i) & (dff['indicator'] == yaxis_column_name)]['value'],
-            text=dff[(dff['indicator'] == yaxis_column_name) & (dff['Continent'] == i)]['Country/Region'],
-            customdata=dff[(dff['indicator'] == yaxis_column_name) & (dff['Continent'] == i)]['Country/Region'],
-            mode='markers',
-            marker={
-                'size': 15,
-                'opacity': 0.5,
-                'line': {'width': 0.5, 'color': 'white'}
-            },
-            name=i
-        ) for i in dff['Continent'].unique()
-        ],
-        'layout': dict(
-            xaxis={
-                'title': xaxis_column_name,
-                'type': 'linear' if xaxis_type == 'Linear' else 'log'
-            },
-            yaxis={
-                'title': yaxis_column_name,
-                'type': 'linear' if yaxis_type == 'Linear' else 'log'
-            },
-            margin={'l': 40, 'b': 30, 't': 10, 'r': 0},
-            height=450,
-            hovermode='closest'
-        ),
-        "id": 'blerg'
-    }
-
-
-def create_time_series(dff, axis_type, title) -> dict:
-    return {
-        'data': [dict(
-            x=dff['Date'],
-            y=dff['value'],
-            mode='lines+markers'
-        )],
-        'layout': {
-            'height': 225,
-            'margin': {'l': 40, 'b': 30, 'r': 10, 't': 10},
-            'annotations': [{
-                'x': 0, 'y': 0.85, 'xanchor': 'left', 'yanchor': 'bottom',
-                'xref': 'paper', 'yref': 'paper', 'showarrow': False,
-                'align': 'left', 'bgcolor': 'rgba(255, 255, 255, 0.5)',
-                'text': title
-            }],
-            'yaxis': {'type': 'linear' if axis_type == 'Linear' else 'log'},
-            'xaxis': {'showgrid': False}
-        }
-    }
-
-
-@app.callback(
-    dash.dependencies.Output('x-time-series', 'figure'),
-    [dash.dependencies.Input('crossfilter-indicator-scatter', 'hoverData'),
-     dash.dependencies.Input('crossfilter-xaxis-column', 'value'),
-     dash.dependencies.Input('crossfilter-xaxis-type', 'value')])
-def update_y_timeseries(hover_data, xaxis_column_name, axis_type):
-    country_name = hover_data['points'][0]['customdata']
-    dff = df[df['Country/Region'] == country_name]
-    dff = dff[dff['indicator'] == xaxis_column_name]
-    title = f'<b>{country_name}</b><br>{xaxis_column_name}'
-    return create_time_series(dff, axis_type, title)
-
-
-@app.callback(
-    dash.dependencies.Output('y-time-series', 'figure'),
-    [dash.dependencies.Input('crossfilter-indicator-scatter', 'hoverData'),
-     dash.dependencies.Input('crossfilter-yaxis-column', 'value'),
-     dash.dependencies.Input('crossfilter-yaxis-type', 'value')]
-)
-def update_x_timeseries(hover_data, yaxis_column_name, axis_type):
-    dff = df[df['Country/Region'] == hover_data['points'][0]['customdata']]
-    dff = dff[dff['indicator'] == yaxis_column_name]
-    return create_time_series(dff, axis_type, yaxis_column_name)
-
-
 if __name__ == '__main__':
-    app.run_server(port=8088)
+    app.run_server()
