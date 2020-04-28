2fd24f5a6406e01a5d226977cba9a842a143cd85
davidjones <david.jones@tnt-digital.com>
2020-04-28 15:46:31 +0200

    Add server line because of bug

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import dash_core_components as dcc
 import dash_html_components as html
 
 app = dash.Dash(__name__)
-
+server = app.server
 app.layout = html.Div(children=[
     html.H1(children='Hello Dash'),
 
